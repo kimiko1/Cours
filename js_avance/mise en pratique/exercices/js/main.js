28f38bbf265ef3c3a6b0116dfb52948b3755558f
bastien <bastien.raoult1pro@gmail.com>
2024-10-22 13:41:14 +0200

    fix

--- js_avance/mise en pratique/exercices/js/main.js
+++ js_avance/mise en pratique/exercices/js/main.js
@@ -1,7 +1,14 @@
-import { Rectangle } from "./classes/Rectangle.js";
-import { Circle } from "./classes/Circle.js";
+import { Rectangle } from "./classes/rectangle.js";
+import { Circle } from "./classes/circle.js";
 
-// On crée une instance de Rectangle
+// // On crée une instance de Rectangle
+// const rectangle = new Rectangle(100, 100, "red", 50, 150);
+// console.log(rectangle);
+
+// // On crée une instance de Rectangle
+// const circle = new Circle(100, 100, "red", 50);
+// console.log(circle);
+
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
@@ -9,8 +16,7 @@ const shapes = [
     new Rectangle(50, 50, "blue", 100, 200),
     new Circle(300, 200, "green", 75),
     new Rectangle(400, 100, "purple", 150, 100),
-    new Rectangle(200, 300, "purple", 150, 100),
-    new Rectangle(600, 400, "purple", 150, 100),
+    new Circle(500, 400, "red", 75),
 ];
 
 // Boucle à travers toutes les formes et dessine-les
